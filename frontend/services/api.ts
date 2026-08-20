3d270d4afc9a3592940006f2280d2c619f23c669
wafathima <wafathima15@gmail.com>
2026-08-20 10:03:16 +0530

    api frontend changed

--- frontend/services/api.ts
+++ frontend/services/api.ts
@@ -1,7 +1,17 @@
 import axios, { AxiosError } from "axios";
 
+// const api = axios.create({
+//   baseURL: process.env.NEXT_PUBLIC_API_URL || "http://localhost:5000/api",
+//   headers: {
+//     "Content-Type": "application/json",
+//   },
+//   timeout: 10000,
+// });
+
 const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL || "http://localhost:5000/api",
+  baseURL:
+    process.env.NEXT_PUBLIC_API_URL ||
+    "http://localhost:5000/api",
   headers: {
     "Content-Type": "application/json",
   },
